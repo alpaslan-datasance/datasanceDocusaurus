5ea44cc08879ec8dd18872bebbf5d07ca3140d1e
Alpaslan DOGAN <alpaslanndogan@hotmail.com>
2024-10-24 00:29:28 +0300

    Datasance yaml updated.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
           // Pass it a path to a local OpenAPI YAML file
           {
             // Redocusaurus will automatically bundle your spec into a single file during the build
-            spec: 'openapi/index.yaml',
+            spec: 'openapi/datasance-api.yaml',
             route: '/api/',
           },
         ],
@@ -83,6 +83,38 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    // algolia: {
+    //   // The application ID provided by Algolia
+    //   appId: 'YOUR_APP_ID',
+
+    //   // Public API key: it is safe to commit it
+    //   apiKey: 'YOUR_SEARCH_API_KEY',
+
+    //   indexName: 'YOUR_INDEX_NAME',
+
+    //   // Optional: see doc section below
+    //   contextualSearch: true,
+
+    //   // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+    //   externalUrlRegex: 'external\\.com|domain\\.com',
+
+    //   // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+    //   replaceSearchResultPathname: {
+    //     from: '/docs/', // or as RegExp: /\/docs\//
+    //     to: '/',
+    //   },
+
+    //   // Optional: Algolia search parameters
+    //   searchParameters: {},
+
+    //   // Optional: path for search page that enabled by default (`false` to disable it)
+    //   searchPagePath: 'search',
+
+    //   // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+    //   insights: false,
+
+    //   //... other Algolia params
+    // },
     navbar: {
       title: 'Datasance',
       logo: {
@@ -101,7 +133,7 @@ const config: Config = {
           position: 'left',
           items: [
             {
-              label: 'All',
+              label: 'Datasance PoT-ioFog Controller (1.0.0)',
               to: '/api',
             }
           ]
